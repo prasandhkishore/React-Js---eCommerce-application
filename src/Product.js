9c15819c1098ef2d141b55b8717023001c1b1b63
prasandhkishore <prasanthpalanisamy5@gmail.com>
2023-10-23 20:02:06 +0530

    Committing new changes

--- src/Product.js
+++ src/Product.js
@@ -1,12 +1,28 @@
+import axios from 'axios';
 import React, { Component } from 'react';
 class Product extends Component {
     constructor(props) {
         super(props);
+        this.state={
+            products:[]
+        }
+    }
+    getAllProduct(){
+        axios.get("http://localhost:3000/Product").then((Response)=>
+        this.setState({
+            products:Response.data
+        })
+        ).catch((error)=>{
+            console.log("Unable to fetch the data")
+        })
     }
     render() { 
+        const product=this.state.products.map((product)=>{
+            
+        })
         return ( 
             <div>
-                <h2 style={{paddingTop:'100px'}}>Hello from Product Page</h2>
+                <h2 style={{paddingTop:'100px'}}>Product List</h2>
             </div>
          );
     }
